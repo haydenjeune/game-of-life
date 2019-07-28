0f6131f74b2af900a33f1dde36d148bf9eac2b65
Hayden Jeune <hayden.jeune@gmail.com>
2019-07-28 17:17:25 +1200

    Add world struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+struct  World {
+    size: usize,
+    elements: Vec<bool>,
+}
+
+impl World {
+    fn new(size: usize) -> World {
+        let elements: Vec<bool> = vec![false; size*size];
+        World {size, elements}
+    }
+
+    fn get(&self, row: &usize, col: &usize) -> Option<&bool> {
+        let idx = self.size * row + col;
+
+        self.elements.get(idx)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let world = World::new(3);
+
+    println!("Size: {}", world.size);
+    println!("Elements: {:?}", world.elements)
+
 }
